ff8add5f6fb870b413527f8f9595aca65bbc0c3e
Canop <cano.petrole@gmail.com>
2024-01-17 20:14:33 +0100

    v2.0, supports combining keys

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,16 @@ use {
 };
 
 pub fn main() {
-    let fmt = KeyEventFormat::default();
+    let fmt = KeyCombinationFormat::default();
     println!("Many terminal applications let you define key bindings but it's hard to know which ones are available.");
     println!("This program shows you the key combinations that can be used in a program running in this terminal.");
+    let mut combiner = Combiner::default();
+    let combines = combiner.enable_combining().unwrap();
+    if combines {
+        println!("Your terminal supports combining keys");
+    } else {
+        println!("Your terminal doesn't support combining standard (non modifier) keys");
+    }
     println!("Type any key combination.");
     loop {
         terminal::enable_raw_mode().unwrap();
@@ -18,20 +25,24 @@ pub fn main() {
         terminal::disable_raw_mode().unwrap();
         match e {
             Ok(Event::Key(key_event)) => {
-                match key_event {
+                let Some(key_combination) = combiner.transform(key_event) else {
+                    continue;
+                };
+                let key = fmt.to_string(key_combination);
+                match key_combination {
                     key!(ctrl-c) => {
-                        println!("You killed me with a {}", fmt.to_string(key_event).red());
+                        println!("You killed me with a {}", key.red());
                         break;
                     }
                     key!(ctrl-q) => {
-                        println!("You typed {} which gracefully quits", fmt.to_string(key_event).green());
+                        println!("You typed {} which gracefully quits", key.green());
                         break;
                     }
                     key!('?') | key!(shift-'?') => {
-                        println!("You typed {} but there's no help in this app", fmt.to_string(key_event).yellow());
+                        println!("You typed {} but there's no help in this app", key.yellow());
                     }
                     _ => {
-                        println!("You typed {}", fmt.to_string(key_event).blue());
+                        println!("You typed {}", key.blue());
                     }
                 }
             }
